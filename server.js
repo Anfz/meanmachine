5a32d077ee8d5288a9a0f7c8f09845ca3bbd3863
Christopher Beaver <chrisbvr@gmail.com>
2015-06-25 11:51:05 +0000

    Admin Router and Middleware up

--- server.js
+++ server.js
@@ -10,8 +10,17 @@ app.get('/', function (req, res) {
 // start the server
 app.listen(8080);// create routes for the admin section
 
-// get an instance of the router
+
+//get an instance of the router
 var adminRouter = express.Router();
+// route middleware that will happen on every request
+adminRouter.use(function(req, res, next) {
+// log each request to the console
+ console.log(req.method, req.url);
+ // continue doing what we were doing and go to the route
+ next();
+});
+
 
 // admin main page. the dashboard (http://localhost:1337/admin)
 adminRouter.get('/', function (req, res) {
