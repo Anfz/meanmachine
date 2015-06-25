426f95b870cbb735dbf32f1ca25d6b4658ac0211
Christopher Beaver <chrisbvr@gmail.com>
2015-06-25 03:14:21 +0000

    Simple Express Server

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('Not Actually My first App, but I am going to pretend it is ok');
+// load the express package and create our app
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// send our index.html file to the user for the home page
+app.get('/', function(req, res) {
+res.sendfile(path.join(__dirname + '/index.html'));
+});
+
+ // start the server
+ app.listen(8080);
+ console.log('1337 is the magic port!');
